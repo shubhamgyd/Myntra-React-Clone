3c95230d4346a56690ed26874a1af03c4051bbe6
shubhamyadav97660@gmail.com <neeta6854@gmail.com>
2023-12-17 19:06:06 +0530

    Header.jsx file created and header code copied and pasted from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,17 @@
+import Header from "./components/Header"
+
 function App() {
   
   return (
     <>
-      <header>
-        <div class="logo_container">
-            <a href="#"><img class="myntra_home" src="images/myntra_logo.webp" alt="Myntra Home"/></a>
-        </div>
-        <nav class="nav_bar">
-            <a href="#">Men</a>
-            <a href="#">Women</a>
-            <a href="#">Kids</a>
-            <a href="#">Home & Living</a>
-            <a href="#">Beauty</a>
-            <a href="#">Studio <sup>New</sup></a>
-        </nav>
-        <div class="search_bar">
-            <span class="material-symbols-outlined search_icon">search</span>
-            <input class="search_input" placeholder="Search for products, brands and more"/>
-        </div>
-        <div class="action_bar">
-            <div class="action_container">
-                <span class="material-symbols-outlined action_icon">person</span>
-                <span class="action_name">Profile</span>
-            </div>
-
-            <div class="action_container">
-                <span class="material-symbols-outlined action_icon">favorite</span>
-                <span class="action_name">Wishlist</span>
-            </div>
-
-            <a class="action_container" href="pages/bag.html">
-                <span class="material-symbols-outlined action_icon">shopping_bag</span>
-                <span class="action_name">Bag</span>
-                <span class="bag-item-count">0</span>
-            </a>
-        </div>
-    </header>
+      <Header />
     <main>
-        <div class="items-container">
+        <div className="items-container">
         </div>
     </main>
     <footer>
-        <div class="footer_container">
-            <div class="footer_column">
+        <div className="footer_container">
+            <div className="footer_column">
                 <h3>ONLINE SHOPPING</h3>
 
                 <a href="#">Men</a>
@@ -54,7 +23,7 @@ function App() {
                 <a href="#">Myntra Insider</a>
             </div>
 
-            <div class="footer_column">
+            <div className="footer_column">
                 <h3>ONLINE SHOPPING</h3>
 
                 <a href="#">Men</a>
@@ -66,7 +35,7 @@ function App() {
                 <a href="#">Myntra Insider</a>
             </div>
 
-            <div class="footer_column">
+            <div className="footer_column">
                 <h3>ONLINE SHOPPING</h3>
 
                 <a href="#">Men</a>
@@ -80,7 +49,7 @@ function App() {
         </div>
         <hr />
 
-        <div class="copyright">
+        <div className="copyright">
             © 2023 www.myntra.com. All rights reserved.
         </div>
     </footer>
--- src/components/Bag.jsx
+++ src/components/Bag.jsx
@@ -2,10 +2,10 @@ const Bag = () => {
   return (
     <div>
       <header>
-        <div class="logo_container">
-            <a href="#"><img class="myntra_home" src="../images/myntra_logo.webp" alt="Myntra Home"/></a>
+        <div className="logo_container">
+            <a href="#"><img className="myntra_home" src="../images/myntra_logo.webp" alt="Myntra Home"/></a>
         </div>
-        <nav class="nav_bar">
+        <nav className="nav_bar">
             <a href="#">Men</a>
             <a href="#">Women</a>
             <a href="#">Kids</a>
@@ -13,40 +13,40 @@ const Bag = () => {
             <a href="#">Beauty</a>
             <a href="#">Studio <sup>New</sup></a>
         </nav>
-        <div class="search_bar">
-            <span class="material-symbols-outlined search_icon">search</span>
-            <input class="search_input" placeholder="Search for products, brands and more"/>
+        <div className="search_bar">
+            <span className="material-symbols-outlined search_icon">search</span>
+            <input className="search_input" placeholder="Search for products, brands and more"/>
         </div>
-        <div class="action_bar">
-            <div class="action_container">
-                <span class="material-symbols-outlined action_icon">person</span>
-                <span class="action_name">Profile</span>
+        <div className="action_bar">
+            <div className="action_container">
+                <span className="material-symbols-outlined action_icon">person</span>
+                <span className="action_name">Profile</span>
             </div>
 
-            <div class="action_container">
-                <span class="material-symbols-outlined action_icon">favorite</span>
-                <span class="action_name">Wishlist</span>
+            <div className="action_container">
+                <span className="material-symbols-outlined action_icon">favorite</span>
+                <span className="action_name">Wishlist</span>
             </div>
 
-            <div class="action_container">
-                <span class="material-symbols-outlined action_icon">shopping_bag</span>
-                <span class="action_name">Bag</span>
-                <span class="bag-item-count">0</span>
+            <div className="action_container">
+                <span className="material-symbols-outlined action_icon">shopping_bag</span>
+                <span className="action_name">Bag</span>
+                <span className="bag-item-count">0</span>
             </div>
         </div>
     </header>
     <main>
-      <div class="bag-page">
-        <div class="bag-items-container">
+      <div className="bag-page">
+        <div className="bag-items-container">
         </div>
-        <div class="bag-summary">
+        <div className="bag-summary">
         </div>
 
       </div>
     </main>
     <footer>
-        <div class="footer_container">
-            <div class="footer_column">
+        <div className="footer_container">
+            <div className="footer_column">
                 <h3>ONLINE SHOPPING</h3>
 
                 <a href="#">Men</a>
@@ -58,7 +58,7 @@ const Bag = () => {
                 <a href="#">Myntra Insider</a>
             </div>
 
-            <div class="footer_column">
+            <div className="footer_column">
                 <h3>ONLINE SHOPPING</h3>
 
                 <a href="#">Men</a>
@@ -70,7 +70,7 @@ const Bag = () => {
                 <a href="#">Myntra Insider</a>
             </div>
 
-            <div class="footer_column">
+            <div className="footer_column">
                 <h3>ONLINE SHOPPING</h3>
 
                 <a href="#">Men</a>
@@ -84,7 +84,7 @@ const Bag = () => {
         </div>
         <hr/>
 
-        <div class="copyright">
+        <div className="copyright">
             © 2023 www.myntra.com. All rights reserved.
         </div>
     </footer>
